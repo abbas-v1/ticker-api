5465be9b38ea21a046ca745d10d97d80d912665b
Muhammad Abbas <abbas@Muhammads-MacBook-Pro.local>
2018-12-31 18:57:26 +0900

    SQL scripts for ticker api

---
+++ USER_table_inserts.sql
@@ -0,0 +1,16 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1671\cocoasubrtf200
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 INSERT INTO APP.USERS (USER_ID, FIRST_NAME, LAST_NAME, REGISTRATION) \
+	VALUES (1, 'Abbas', 'Khan', '2018-12-30 13:20:46.555');\
+INSERT INTO APP.USERS (USER_ID, FIRST_NAME, LAST_NAME, REGISTRATION) \
+	VALUES (2, 'Bilal', 'Ahmed', '2018-12-30 13:20:58.709');\
+INSERT INTO APP.USERS (USER_ID, FIRST_NAME, LAST_NAME, REGISTRATION) \
+	VALUES (3, 'John', 'Doe', '2018-12-30 13:21:16.389');\
+INSERT INTO APP.USERS (USER_ID, FIRST_NAME, LAST_NAME, REGISTRATION) \
+	VALUES (4, 'Khan', 'Muhammad', '2018-12-30 13:21:42.396');\
+}
